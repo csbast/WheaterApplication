85fe3dffb2dd91d9c3847b5ff415fb1986285dc0
csbast <csb.dev@outlook.com>
2022-03-09 21:41:26 -0300

    added dataBinding

--- app/src/main/java/com/example/wheaterapplication/presentation/localweather/LocalWeatherFragment.kt
+++ app/src/main/java/com/example/wheaterapplication/presentation/localweather/LocalWeatherFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.wheaterapplication.R
+import com.example.wheaterapplication.databinding.FragmentLocalWeatherBinding
 
 class LocalWeatherFragment : Fragment() {
+
+    private var _binding: FragmentLocalWeatherBinding? = null
+    private val binding: FragmentLocalWeatherBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_local_weather, container, false)
+    ): View {
+        _binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_local_weather, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
